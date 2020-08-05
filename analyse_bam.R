c484982223eb85cfe54e0cd82d0755f42a9f62b5
Priit Paluoja <priitpaluoja@gmail.com>
2020-08-05 13:03:05 +0300

    Implement chi-squared variation transformation, add command line args and document

--- analyse_bam.R
+++ analyse_bam.R
@@ -2,27 +2,27 @@ library(Rsamtools)
 library(tidyverse)
 library(GenomicAlignments)
 
-reference_location <- "reference.tsv"
-bam_location <- "C:/Users/Priit/Dropbox/Informaatika/Helsingi Ülikool/Käsikiri 2/CNV/bams/A749N.bam"
-syndrome_bed <- "coordinates/as_pws.bed"
-analyze_locations_bed <- "coordinates/chr15.bed"
+args = commandArgs(trailingOnly=TRUE)
+
+# Check that at least three arguments are supplied.
+if (length(args) != 3) {
+  stop("Please provide .bam, .bed and reference locations.", call.=FALSE)
+}
+
+bam_location <- args[1]
+analyze_locations_bed <- args[2]
+reference_location <- args[3]
 
 
 sample <- readGAlignments(bam_location)
 bam_name <- basename(bam_location)
 
 
-syndrome_coordinates <- read_tsv(syndrome_bed) %>% 
-  mutate(region = "syndrome")
-
-analyze_coordinates <- read_tsv(analyze_locations_bed) %>% 
+# Coordinates for use
+locations <- read_tsv(analyze_locations_bed) %>% 
   mutate(region = "reference")
 
-locations <- analyze_coordinates %>% 
-  anti_join(syndrome_coordinates, by = c("chromosome", "start", "end")) %>% 
-  bind_rows(syndrome_coordinates)
-
-
+# Count reads from the sample under analysis.
 counts <- locations %>% 
   mutate(reads = assay(summarizeOverlaps(makeGRangesFromDataFrame(.), sample, mode="IntersectionStrict"))) %>% 
   mutate(normalized_by_sample = reads/sum(reads)) %>% 
@@ -31,13 +31,41 @@ counts <- locations %>%
   
 
 reference <- read_tsv(reference_location) 
+
+ref_size <- reference %>% 
+  select(sample) %>% 
+  distinct() %>% 
+  nrow(.)
+
+total_bins <- reference %>% 
+  nrow(.) / ref_size
   
-  
+# Degrees of freedom
+df <- ref_size - 1
+
+# We will add sample under analysis.
+df <- df + 1
+
+# Z-score calculation with reference.
 results <- reference %>% 
   bind_rows(counts) %>% 
   group_by(chromosome, start, end) %>% 
-  mutate(z_score = (normalized_by_sample - mean(normalized_by_sample))/sd(normalized_by_sample)) %>% 
+  mutate(z_score_over_ref = (normalized_by_sample - mean(normalized_by_sample)) / sd(normalized_by_sample)) %>% 
+  ungroup() %>% # Chi squared: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5431782/
+  group_by(chromosome, start, end) %>%  # the observed read counts are first normalized by multiplying them with a normalization factor. 
+  mutate(factor = sum(reads) / (ref_size * total_bins)) %>% # Factor numerator (sum of specific bin divided by ...)
   ungroup() %>% 
+  group_by(sample) %>% 
+  mutate(factor = factor / (sum(reads) / total_bins)) %>% # Numerator / (the denominator: sum of sample reads divided by...)
+  ungroup() %>% 
+  mutate(on = factor * reads) %>% # Normalized counts
+  select(-factor) %>%  # Factor is not needed
+  group_by(chromosome, start, end) %>% 
+  mutate(expected_on = mean(on)) %>% # Expected value is the average of each bin
+  ungroup() %>% 
+  mutate(chi_squared = ((expected_on - on)^2) / expected_on) %>% # Calculate chi-squared
+  mutate(chi_squared_standard_normal_z_score = (chi_squared - df) / sqrt(2 * df)) %>% # transform to a standard normal distribution N(0, 1) %>% 
   filter(!is.na(type)) %>% 
-  select(-type)
-
+  select(-type) %>% 
+  mutate(z_score_sample_within = (normalized_by_sample - mean(normalized_by_sample)) / sd(normalized_by_sample)) %>% 
+  write_tsv(paste0("results.", bam_name,".tsv"))
