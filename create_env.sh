95be27efc2c085f27239bd38aebbfe27677d7b76
Priit Paluoja <priitpaluoja@gmail.com>
2020-08-06 13:13:58 +0300

    Add conda environment creation script

---
+++ create_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+conda create -n detector bioconductor-rsamtools r-tidyverse r-fs bioconductor-genomicalignments bioconductor-bsgenome.hsapiens.ucsc.hg38
+
