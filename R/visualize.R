370efe5fa323c31d4d85185589afd5a1d1df59b6
Priit Paluoja <priitpaluoja@gmail.com>
2020-08-17 11:46:25 +0300

    update plotting

--- R/visualize.R
+++ R/visualize.R
@@ -1,6 +1,14 @@
 library(tidyverse)
 library(grid)
 
+
+start_x <- -15
+end_x <- 15
+
+start_y <- -15
+end_y <- 15
+
+
 args = commandArgs(trailingOnly = TRUE)
 
 if (length(args) != 2) {
@@ -16,9 +24,7 @@ regions_of_interest_location = args[2]
 
 results <- read_tsv(results_location) %>%
   left_join((
-    read_tsv(regions_of_interest_location) %>%
-      mutate(region = "Focus")
-  )
+    read_tsv(regions_of_interest_location))
   , by =  c("chromosome", "start", "end"))
 
 
@@ -65,111 +71,69 @@ theme <- theme_bw() +
 # Box plots
 ###############
 z.score.ref.plot <-
-  ggplot(results, aes(x = region, y = z_score_ref)) +
-  geom_boxplot(aes(color = region)) +
+  ggplot(results, aes(x = focus, y = z_score_ref)) +
+  geom_boxplot(aes(color = focus)) +
   ggtitle("Z-score ref") +
   theme +
-  scale_y_continuous(limits = c(-10, 10, 1))
+  scale_y_continuous(limits = c(start_y, end_y, 1))
 
 
 z.score.local.plot <-
-  ggplot(results, aes(x = region, y = local_z_score)) +
-  geom_boxplot(aes(color = region)) +
+  ggplot(results, aes(x = focus, y = local_z_score)) +
+  geom_boxplot(aes(color = focus)) +
   ggtitle("Z-score local") +
   theme +
-  scale_y_continuous(limits = c(-10, 10, 1))
+  scale_y_continuous(limits = c(start_y, end_y, 1))
 
 
 zz.score.plot <-
-  ggplot(results, aes(x = region, y = zz_score)) +
-  geom_boxplot(aes(color = region)) +
+  ggplot(results, aes(x = focus, y = zz_score)) +
+  geom_boxplot(aes(color = focus)) +
   ggtitle("ZZ-score") +
   theme +
-  scale_y_continuous(limits = c(-10, 10, 1))
+  scale_y_continuous(limits = c(start_y, end_y, 1))
 
 
-# Scatter plots
+# Density plots
 ###############
 
-# Z-score ref scatter
-z_score_ref.na_mean <- results %>%
-  filter(is.na(region)) %>%
-  select(z_score_ref) %>%
-  summarise(mean(z_score_ref))
-
-z_score_ref.syndrome_mean <- results %>%
-  filter(!is.na(region)) %>%
-  select(z_score_ref) %>%
-  summarise(mean(z_score_ref))
-
-
-z.score.ref.scatter <-
-  ggplot(results, aes(x = start, y = z_score_ref)) +
-  geom_point(aes(color = region), size = 0.5) +
-  geom_hline(yintercept = z_score_ref.na_mean$`mean(z_score_ref)`, color =
-               "blue") +
-  geom_hline(yintercept = z_score_ref.syndrome_mean$`mean(z_score_ref)`, color =
-               "red") +
+# Z-score
+z.score.ref.dens <-
+  ggplot(results, aes(x = z_score_ref)) +
+  geom_density(aes(color = focus), size = 0.5) +
   ggtitle("Z-score ref") +
   theme +
-  scale_y_continuous(limits = c(-10, 10, 1))
+  scale_x_continuous(limits = c(start_x, end_x, 1))
 
 # Z-score local scatter
-z.score.local.na_mean <- results %>%
-  filter(is.na(region)) %>%
-  select(local_z_score) %>%
-  summarise(mean(local_z_score))
-
-z.score.local.syndrome_mean <- results %>%
-  filter(!is.na(region)) %>%
-  select(local_z_score) %>%
-  summarise(mean(local_z_score))
-
-z.score.local.scatter <-
-  ggplot(results, aes(x = start, y = local_z_score)) +
-  geom_point(aes(color = region), size = 0.5) +
-  geom_hline(yintercept = z.score.local.na_mean$`mean(local_z_score)`,
-             color = "blue") +
-  geom_hline(yintercept = z.score.local.syndrome_mean$`mean(local_z_score)`,
-             color = "red") +
+z.score.local.dens <-
+  ggplot(results, aes(x = local_z_score)) +
+  geom_density(aes(color = focus), size = 0.5) +
   ggtitle("Z-score local") +
   theme +
-  scale_y_continuous(limits = c(-10, 10, 1))
+  scale_x_continuous(limits = c(start_x, end_x, 1))
 
-# ZZ-score  scatter
-zz.score.na_mean <- results %>%
-  filter(is.na(region)) %>%
-  select(zz_score) %>%
-  summarise(mean(zz_score))
-
-zz.score.syndrome_mean <- results %>%
-  filter(!is.na(region)) %>%
-  select(zz_score) %>%
-  summarise(mean(zz_score))
-
-zz.score.scatter <-
-  ggplot(results, aes(x = start, y = zz_score)) +
-  geom_point(aes(color = region), size = 0.5) +
-  geom_hline(yintercept = zz.score.na_mean$`mean(zz_score)`,
-             color = "blue") +
-  geom_hline(yintercept = zz.score.syndrome_mean$`mean(zz_score)`,
-             color = "red") +
+# ZZ-score
+zz.score.dens <-
+  ggplot(results, aes(x = zz_score)) +
+  geom_density(aes(color = focus), size = 0.5) +
   ggtitle("ZZ score") +
   theme +
-  scale_y_continuous(limits = c(-10, 10, 1))
+  scale_x_continuous(limits = c(start_x, end_x, 1))
 
 sample_name <- basename(results_location)
+
 
 pdf(file = paste0(sample_name, ".pdf"), title = sample_name)
 multiplot(
   z.score.local.plot,
-  z.score.local.scatter,
+  z.score.local.dens,
   
   z.score.ref.plot,
-  z.score.ref.scatter,
+  z.score.ref.dens,
   
   zz.score.plot,
-  zz.score.scatter,
+  zz.score.dens,
   
   cols = 3
 )
