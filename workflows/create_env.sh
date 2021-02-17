4802a6cff6139e377e468a50edf406c885359a42
priitpaluoja <priitpaluoja@gmail.com>
2021-02-17 10:02:57 +0200

    format as package

--- workflows/create_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exit when any command fails
-set -e
-
-conda create -n detector bioconductor-rsamtools r-tidyverse r-fs bioconductor-genomicalignments bioconductor-bsgenome.hsapiens.ucsc.hg38 cromwell hcc
-
