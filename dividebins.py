a002f3b43f4ada5f05071db0633b6ec357882f1f
Priit Paluoja <priitpaluoja@gmail.com>
2021-10-15 12:19:19 +0300

    Create dividebins.py

---
+++ dividebins.py
@@ -0,0 +1,29 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Generate bins.')
+
+parser.add_argument('--infile',
+                    type=argparse.FileType('r', encoding="UTF-8"),
+                    help="File in the TSV format (UTF-8) with columns: 'chr', 'start', 'end', 'focus', 'length'.")
+
+parser.add_argument('--outfile',
+                    nargs="?",
+                    type=argparse.FileType('w', encoding="UTF-8"),
+                    default=sys.stdout,
+                    help="Binned output file.")
+
+args = parser.parse_args()
+
+header = args.infile.readline()
+args.outfile.write(header.strip() + "\n")
+for line in args.infile:
+    line = line.strip().split("\t")
+    chromosome, start, end, focus, bin_width = line[0], int(line[1]), int(line[2]), line[3], int(line[4])
+
+    while start + bin_width < end:
+        args.outfile.write(f"{chromosome}\t{start}\t{(min(start + bin_width - 1, end))}\t{focus}\n")
+        start = start + bin_width
+
+args.infile.close()
+args.outfile.close()
